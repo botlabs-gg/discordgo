38edc425c7ef2eaaa66e146b186d00a1913fb960
Ashish Jhanwar <ashishjh-bst@users.noreply.github.com>
2022-03-21 19:47:59 +0530

    Updated endpoint, added message intent

--- gateway.go
+++ gateway.go
@@ -110,6 +110,9 @@ const (
 
 	GatewayIntentDirectMessageTyping GatewayIntent = 1 << 14
 	// - TYPING_START
+
+	GatewayIntentMessageContent GatewayIntent = 1 << 15
+	// - MESSAGE_CONTENT
 )
 
 // max size of buffers before they're discarded (e.g after a big incmoing event)
